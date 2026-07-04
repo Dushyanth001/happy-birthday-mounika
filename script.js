bf2fe315ddfb33b1eb653c2296b221e66ba06927
Dushyanth001 <110011475+Dushyanth001@users.noreply.github.com>
2026-07-04 18:49:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.onload = function () {
         if (loader) loader.style.display = "none";
     }, 3000);
 
-    const music = document.getElementById("bgMusic");
+    const music = document.getElementById("bgmusic");
     if (music) {
         music.play().catch(() => {});
     }
